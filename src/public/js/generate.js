ccc011c4f466b773101093dbefced5d7b23e4731
whoskelos KDE Neon <kelvinguerrero2199@gmail.com>
2023-04-20 20:55:35 +0200

    control de posibles inputs erroneos, added icono home, cambiado size del textarea

--- src/public/js/generate.js
+++ src/public/js/generate.js
@@ -17,7 +17,8 @@ const cargarPrompt = (animal) => {
 };
 
 const generateNames = () => {
-    const textPrompt = document.querySelector("#textPrompt").value;
+    regexPrompt = /(Tell me three names for my) (dog|cat)+[a-z ]/i;
+    const textPrompt = document.querySelector("#textPrompt").value.trim();
     const prompt = {
         description: textPrompt,
     };
@@ -25,9 +26,19 @@ const generateNames = () => {
         textPrompt == "Tell me three names for my dog" ||
         textPrompt == "Tell me three names for my cat"
     ) {
-        console.log("Describe your pet please!");
-    } else {
-        console.log(textPrompt);
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please, describe your pet!',
+          })
+    } else if (textPrompt == "" || !regexPrompt.test(textPrompt)) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Something went wrong!',
+        })
+        document.querySelector("#textPrompt").value = `Tell me three names for my ${animal}`;
+    }else {
         document.getElementById("loader").classList.add("answers");
         fetch("/message", {
             method: "POST",
@@ -42,18 +53,21 @@ const generateNames = () => {
 };
 
 const showNames = (names) => {
+    if (document.getElementById("divCards")) {
+        document.getElementById("divCards").remove();
+    }
     //selecciono div donde voy a crear las tarjetas de las respuestas
     const divAnswers = document.getElementById("divNames");
     //quito el spin del loading de la peticion
-    document.getElementById("loader").remove();
-    const response = names.message;
+    document.getElementById("loader").classList.remove("answers");
+    const response = names.message.trim();
     //separo las respuestas para hacer una tarjeta por cada una de ellas
-    const words = response.split(".");
-
+    const words = response.split("\n");
     //creo el div donde van a ir las cards
     const $cards = document.createElement("div");
+    $cards.id = "divCards";
     
-    for (let i = 1; i < words.length; i++) {
+    for (let i = 0; i < words.length; i++) {
         $cards.innerHTML += `<div class="cardAnswer justify-content-between justify-content-md-around shadow-sm mb-3 p-3">
             <img src="/img/${animal}.png" alt="Imagen de ${animal}">
             <p class="fs-4">${words[i]}</p>
